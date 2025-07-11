622208263b37378c88bcc0615026ec5c27b8e4fa
Andrej Erjavec <erandrej@gmail.com>
2025-07-11 12:16:47 +0200

    merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import {provideHttpClient} from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import {MarkdownModule} from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(),
-  ]
+    importProvidersFrom(MarkdownModule.forRoot()),
+  ],
 };
 
-export const WEATHER_API_BASE_URL = 'http://localhost:8083/weather/api';
-export const ACCU_API_BASE_URL = 'http://localhost:8083/accu/api';
+export const WEATHER_API_BASE_URL = 'http://localhost:80/weather/api';
+export const WEATHER_API_BASE_URL_PROD ="https://weather-forecast-backend.greenisland-0015b907.westeurope.azurecontainerapps.io/weather/api";
--- src/app/weather-data-view/weather-data.view.ts
+++ src/app/weather-data-view/weather-data.view.ts
@@ -1,19 +1,21 @@
-import { WEATHER_API_BASE_URL, ACCU_API_BASE_URL } from '../app.config';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { response } from '../data/response';
 import { format } from 'date-fns';
+import { WEATHER_API_BASE_URL_PROD } from '../app.config';
+import {MarkdownComponent} from 'ngx-markdown';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'weather-data-view',
-  imports: [FormsModule],
+  imports: [RouterLink, FormsModule, MarkdownComponent, MarkdownModule],
   templateUrl: './weather-data.view.html',
   styleUrl: './weather-data.view.less',
 })
 export class WeatherDataView {
   http = inject(HttpClient);
+  data: any;
 
   constructor() {}
 
@@ -25,13 +27,13 @@ export class WeatherDataView {
 
   submitLocation() {
     console.log('Location entered:', this.location);
-    const url = `${WEATHER_API_BASE_URL}/${this.location}`;
+    const url = `${WEATHER_API_BASE_URL_PROD}/${this.location}`;
     console.log('Fetching weather data from:', url);
 
     this.http.get<any>(url).subscribe({
       next: (res) => {
         console.log('API response:', res);
-        this.response = res;
+        this.data = this.transformForecast(res);
       },
       error: (err) => {
         console.error('API error:', err);
@@ -39,5 +41,48 @@ export class WeatherDataView {
     });
   }
 
-  response = response;
+  transformForecast(response: any) {
+    const data: any = {
+      city: response.city,
+      forecast: [],
+      openAiAdvice: response.openAiAdvice,
+    };
+
+    // Map to group forecasts by date
+    const dateMap: Map<string, any[]> = new Map();
+
+    for (const forecast of response.data) {
+      if (!dateMap.has(forecast.date)) {
+        dateMap.set(forecast.date, []);
+      }
+
+      // Push forecast with provider info into the correct date group
+      dateMap.get(forecast.date)?.push({
+        provider: forecast.provider,
+        minTemp: forecast.minTemp,
+        maxTemp: forecast.maxTemp,
+        windSpeed: forecast.windSpeed,
+        totalPrecipitationMm: forecast.totalPrecipitationMm,
+        chanceOfRain: forecast.chanceOfRain,
+        humidity: forecast.humidity,
+      });
+    }
+
+    // Convert map to array
+    for (const [date, forecasts] of dateMap.entries()) {
+      data.forecast.push({
+        date,
+        forecast: forecasts,
+      });
+    }
+
+    // Optional: sort by date
+    data.forecast.sort(
+      (a: any, b: any) =>
+        new Date(a.date).getTime() - new Date(b.date).getTime()
+    );
+
+    console.log(data);
+    return data;
+  }
 }
