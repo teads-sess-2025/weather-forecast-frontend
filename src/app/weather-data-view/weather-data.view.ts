2c0371a6a3fd2af273dd952dec4ca5d113919318
Andrej Erjavec <erandrej@gmail.com>
2025-07-11 14:44:13 +0200

    button loading indicator

--- src/app/weather-data-view/weather-data.view.ts
+++ src/app/weather-data-view/weather-data.view.ts
@@ -20,6 +20,7 @@ export class WeatherDataView {
   constructor() {}
 
   public location = '';
+  loading = false
 
   providers: Record<string, string> = {
     "weather_api": "Weather API",
@@ -31,6 +32,7 @@ export class WeatherDataView {
   }
 
   submitLocation() {
+    this.loading = true;
     console.log('Location entered:', this.location);
     const url = `${WEATHER_API_BASE_URL_PROD}/${this.location}`;
     console.log('Fetching weather data from:', url);
@@ -40,9 +42,11 @@ export class WeatherDataView {
         console.log('API response:', res);
         this.data = this.transformForecast(res);
         console.log(this.data);
+        this.loading = false;
       },
       error: (err) => {
         console.error('API error:', err);
+        this.loading = false;
       },
     });
   }
